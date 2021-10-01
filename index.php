96127ea35efb1a99a89af0979687e5253632f74f
unknown <williamboschetti20@gmail.com>
2021-10-01 12:58:38 -0300

    metodo de lista

--- index.php
+++ index.php
@@ -2,9 +2,30 @@
 
 require_once("config.php");
 
-$usuario = new Usuario();
+// Pegar um usuario $usuario = new Usuario();
+//$usuario->loadById(4);
 
-$usuario->loadById(4);
+//echo $usuario;
+
+
+//Carrega uma lista de usuario
+
+//$list = Usuario::getList();
+
+//echo json_encode($list);
+
+
+//Carrega uma lita de usuarios buscando pelo login
+
+//$busca = Usuario::search("ro");
+
+//echo json_encode($busca);
+
+
+//Carrega um usuario usando um login e senha
+
+$usuario = new Usuario();
+$usuario->login("root", "123456");
 
 echo $usuario;
 
